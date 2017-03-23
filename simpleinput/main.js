4304c998c0f2fbb1bd13c831dc5c560f2bf8aba8
ryanjwatts <ryanjwatts@gmail.com>
2017-03-23 10:15:00 -0700

    add simpleinput folder files

---
+++ simpleinput/main.js
@@ -0,0 +1,7 @@
+function displayText() {
+    const input = document.getElementById('input')
+    const dropdown = document.getElementById('picker')
+    const output = document.getElementById('display')
+    output.innerHTML = picker.value;
+    // console.log(input.onkeyup)
+}
